f2a5f4ba5a6d08595ddc66995126a79479ee721c
Resalia <50954859+Shtraiy@users.noreply.github.com>
2024-10-29 16:39:00 +0800

    Update omz.sh

--- oh-my-zsh/omz.sh
+++ oh-my-zsh/omz.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cp -f .zshrc ~/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+chsh -s /bin/zsh
+
+cp -f .zshrc ~/
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
