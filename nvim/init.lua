52db4b5355134b6dc68a9c2c210f82606f3f400d
Resalia <weihuazhen24@Gmail.com>
2024-11-08 10:34:05 +0800

    2024/11/8

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,7 @@ vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
 vim.cmd([[highlight IncSearch guifg=#f9f5d7 ]])
 vim.cmd([[highlight Search guifg=#f9f5d7 ]])
+
+vim.cmd([[highlight IlluminatedWordText gui=underline ]])
+vim.cmd([[highlight IlluminatedWordRead gui=underline ]])
+vim.cmd([[highlight IlluminatedWordWrite gui=underline ]])
