e943d6cc2104479ebf89b64ee46c252f91dd289c
Resalia <weihuazhen24@Gmail.com>
2024-11-14 23:45:01 +0800

    2024/11/14

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,8 @@
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
-vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-vim.cmd([[highlight IncSearch guifg=#f9f5d7 ]])
-vim.cmd([[highlight Search guifg=#f9f5d7 ]])
+require("config.highlight")
 
 
--- Illuminate配置
-vim.cmd([[highlight IlluminatedWordWrite gui=underline ]])
-vim.cmd([[highlight IlluminatedWordText gui=none ]])
-vim.cmd([[highlight IlluminatedWordRead gui=underline ]])
-vim.cmd([[highlight Function guifg=#30db97 ]])
+
+
---
+++ nvim/lua/config/highlight.lua
@@ -0,0 +1,17 @@
+-- 背景配置
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+
+-- 高亮组配置
+
+-- Flash配置
+vim.cmd([[highlight IncSearch guifg=#f9f5d7 ]])
+vim.cmd([[highlight Search guifg=#f9f5d7 ]])
+
+
+-- Illuminate配置
+vim.cmd([[highlight IlluminatedWordWrite gui=underline ]])
+vim.cmd([[highlight IlluminatedWordText gui=none ]])
+vim.cmd([[highlight IlluminatedWordRead gui=underline ]])
+vim.cmd([[highlight Function guifg=#30db97 ]])
